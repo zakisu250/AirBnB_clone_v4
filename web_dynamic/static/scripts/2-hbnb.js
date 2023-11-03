15befcf6d5ad7963c4fcae7017629a2cdf7ad2ec
zakisu250 <zakisu250@gmail.com>
2023-11-03 10:10:13 +0300

    variable referencing correction for HOST

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 $(document).ready(init);
 
-const HOST = '0.0.0.0'
+const HOST = '0.0.0.0';
 function init () {
   const amenityDict = {}
   $('.amenities .popover input').change(function () {
@@ -16,7 +16,7 @@ function init () {
 }
 
 function apiStat () {
-  const API_URL = `http://$(HOST):5001/api/v1/status/`;
+  const API_URL = `http://${HOST}:5001/api/v1/status/`;
   $.get(API_URL, (data, textStatus) => {
     if (textStatus === 'success' && data.status === 'OK') {
       $('#api_status').addClass('available');
