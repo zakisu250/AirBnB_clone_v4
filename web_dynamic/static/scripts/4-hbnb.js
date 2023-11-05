4a04b2632c706ea74a4fb8c993a441f6b5046c65
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-11-05 11:16:28 +0300

    semistandard fix

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,5 +1,5 @@
+const HOST = '127.0.0.1';
 $(document).ready(function () {
-  const HOST = '0.0.0.0';
   const amenityDict = {};
   $('.amenities .popover input').click(function () {
     if ($(this).prop('checked')) {
@@ -10,7 +10,11 @@ $(document).ready(function () {
     const amenKeys = Object.keys(amenityDict);
     $('.amenities h4').text(amenKeys.sort().join(', '));
   });
+  statusCheck();
+  searchPlaces();
+});
 
+function statusCheck () {
   const statusURL = `http://${HOST}:5001/api/v1/status/`;
   $.get(statusURL, function (data) {
     if (data.status === 'OK') {
@@ -19,7 +23,9 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
+}
 
+function searchPlaces () {
   $('button').on('click', function () {
     const placeURL = `http://${HOST}:5001/api/v1/places_search/`;
     const amenityIds = $('.amenities .popover input:checked').map((i, e) => $(e).data('id')).get();
@@ -31,18 +37,19 @@ $(document).ready(function () {
       success: function (res) {
         let articles = '';
         for (const r of res) {
-          const article = `<article>
-         <div class="title_box">
-            <h2>${r.name}</h2>
-            <div class="price_by_night">$${r.price_by_night}</div>
-          </div>
-          <div class="information">
+          const article =
+           `<article>
+           <div class="title_box">
+             <h2>${r.name}</h2>
+             <div class="price_by_night">$${r.price_by_night}</div>
+           </div>
+           <div class="information">
             <div class="max_guest">${r.max_guest} Guest(s)</div>
             <div class="number_rooms">${r.number_rooms} Bedroom(s)</div>
             <div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>
-          </div>
-          <div class="description">${r.description}</div>
-        </article>`;
+           </div>
+           <div class="description">${r.description}</div>
+           </article>`;
           articles += article;
         }
         $('section.places').html(articles);
@@ -52,4 +59,4 @@ $(document).ready(function () {
       }
     });
   });
-});
+}
